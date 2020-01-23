b9b9a5997962cac420ba2d16ae476828508f21e7
joelamadotdx <joel.amado@talkdesk.com>
2020-01-23 17:24:03 +0000

    merged

---
+++ src/main/java/com/talkdesk/challenge/RestApplication.java
@@ -0,0 +1,8 @@
+package com.talkdesk.challenge;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/data")
+public class RestApplication  extends Application {
+}
